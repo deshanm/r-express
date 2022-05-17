ad9de1e96f19dfc75279bf72617f633a97e75792
deshanm <madurajithdeshan@gmail.com>
2022-05-17 22:55:38 +0530

    bug-version: renamed pet to user to reproduce the bug

--- examples/mvc/controllers/pet/index.js
+++ examples/mvc/controllers/pet/index.js
@@ -25,7 +25,7 @@ exports.edit = function(req, res, next){
 
 exports.update = function(req, res, next){
   var body = req.body;
-  req.pet.name = body.pet.name;
+  req.pet.name = body.user.name;
   res.message('Information updated!');
   res.redirect('/pet/' + req.pet.id);
 };
